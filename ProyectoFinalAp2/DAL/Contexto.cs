bd86751d4fec1a9ac7f53803650393fb0b09dc8b
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-06-10 18:35:01 -0400

    Add project files.

---
+++ ProyectoFinalAp2/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAp2.DAL
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Productos> Productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\Agrosoft.db");
+        }
+
+    }
+}
